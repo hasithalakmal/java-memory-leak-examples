b7299d32e86112c6fd40e1c80cd2d96ef71f3b34
hasithag <hasitha.gamage@sysco.com>
2019-11-18 07:11:14 +0530

    sample implementations

--- src/main/java/com/smile/service/StaticFieldHolding.java
+++ src/main/java/com/smile/service/StaticFieldHolding.java
@@ -6,8 +6,7 @@ import java.util.Random;
 public class StaticFieldHolding {
 
     private Random random = new Random();
-    public static final ArrayList<Double> list
-          = new ArrayList<>(1000000);
+    public static final ArrayList<Double> list = new ArrayList<>(1000000);
 
     public void populateData() {
         for (int i = 0; i < 1000000; i++) {
